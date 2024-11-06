8b73ab9f700641d5199b9e5c879f80991a53e639
developer-hassan <hassanasim5567@gmail.com>
2024-11-06 10:07:58 +0500

    initial commit - version 3

--- helper_functions.py
+++ helper_functions.py
@@ -21,7 +21,8 @@ from output_attributes import (
     extract_modello_a_serie,
     extract_model_part_b,
     calculate_accuracy,
-    extract_modello_a_serie_and_stage
+    extract_modello_a_serie_and_stage,
+    extract_family_from_model
 )
 
 from langchain_service.agent import LangchainAgent
@@ -197,132 +198,8 @@ def load_model_data() -> pd.DataFrame:
 def load_input_data(brand_name: str) -> pd.DataFrame:
     file_path = os.path.join(INPUT_DIR, f'VehicleToOrganize_{brand_name}.xlsx')
     df = pd.read_excel(file_path)
-    df = df.head(500)
+    # df = df.head(500)
     return df
-
-
-def handle_no_family_name_rows(
-        no_family_name_rows, no_family_name_descriptions, valid_family_names, brand_data, possible_brands,
-        df_family, model_df, agent_vehicles_organized, agent_ai_vehicles_organized
-):
-    print("AGENT CALLED")
-    responses = []
-    try:
-        print(f"Total number of records to be handled by agent: {len(no_family_name_rows)}")
-
-        # Initialize the descriptions list with the original data
-        descriptions_list = no_family_name_descriptions
-        agent = LangchainAgent()
-
-        request_number = 1
-        while len(descriptions_list) > 300:
-            # Split the current descriptions into a chunk of 300 and the rest
-            descriptions_chunk = descriptions_list[:300]
-            descriptions_list = descriptions_list[300:]
-
-            # Get response from the agent for the current chunk
-            log.info(f"Making Groq request number: {request_number}")
-            chunk_response = agent.get_response(valid_names=valid_family_names, descriptions=descriptions_chunk)
-
-            start_index = chunk_response.find('{')
-            end_index = chunk_response.rfind('}')
-
-            json_data = chunk_response[start_index:end_index + 1]
-            json_data = re.split(r',\s*\n', json_data)
-
-            try:
-                log.info(f"Got chunk response: {json_data}")
-            except Exception as e:
-                log.error(f"Exception in printing the chunk response: {str(e)}")
-            else:
-                # Add the response to the overall responses list
-                responses.extend(json_data)
-                request_number += 1
-
-        # Process any remaining descriptions (less than or equal to 300)
-        if descriptions_list:
-            log.info(f"Making Groq request number: {request_number}")
-            final_response = agent.get_response(valid_names=valid_family_names, descriptions=descriptions_list)
-            start_index = final_response.find('{')
-            end_index = final_response.rfind('}')
-            json_data = final_response[start_index:end_index + 1]
-            json_data = re.split(r',\s*\n', json_data)
-            responses.extend(json_data)
-    except Exception as e:
-        print(f"Exception during LLM Calls: {e}")
-
-    log.debug(f"Total number of 'no_family_name_rows' is: {len(no_family_name_rows)}")
-    log.debug(f"Total number of 'responses' is: {len(responses)}")
-
-    try:
-        for row, response in zip(no_family_name_rows, responses):
-            try:
-                response = json.loads(response)
-                # print(f"Decoded JSON response: {response}")
-            except json.JSONDecodeError as e:
-                log.error(f"Error decoding JSON response: {response}, REASON: {str(e)}")
-                try:
-                    response_corrected = re.sub(r'(?<=: )None(?=[,}])', '"None"', response)
-                    response = json.loads(response_corrected)
-                    log.debug(f"Corrected JSON response for None case: {response}")
-                except Exception as e:
-                    log.error(f"Error decoding JSON response: INNER EXCEPTION, REASON: {str(e)}")
-                    continue
-            except Exception as e:
-                log.error(f"Error decoding JSON response: {response}, REASON: {str(e)}")
-                continue
-
-            family_name = 'None'
-            if 'extracted_name' in response:
-                family_name = response['extracted_name']
-                # Perform the action when 'extracted_name' is present
-                row['Mod_Familigoa'] = family_name
-            else:
-                # Handle the case where 'extracted_name' is not present
-                row['Mod_Familigoa'] = 'Not Returned'
-            year = extract_year(row['Modello_Descriz_Originale'])
-            brand, brand_record_id = extract_brand(row['Modello_Descriz_Originale'], brand_data, year,
-                                                   possible_brands=possible_brands)
-
-            family_record_id = extract_family_id(family_name, year, df_family)
-            modello_a_serie, id_record_model, mod_stage, mod_serie = extract_modello_a_serie(
-                family_name, model_df,
-                year,
-                row['Modello_Descriz_Originale'],
-                brand
-            )
-            modello_b_versione = extract_model_part_b(
-                row['Modello_Descriz_Originale'], year, brand, family_name,
-                modello_a_serie, mod_serie, stage=mod_stage
-            )
-            accuracy = calculate_accuracy(
-                brand, family_name, modello_a_serie, year, brand_record_id,
-                family_record_id, id_record_model, mod_serie, mod_stage
-            )
-
-            append_to_vehicles(
-                vehicles_list=agent_vehicles_organized, cod_veicolo=row['CodVeicolo'], year=year, brand=brand,
-                modello_a_serie=modello_a_serie, modello_b_versione=modello_b_versione,
-                mod_verificato=row['Mod_Verificato'], family_name=family_name, mod_versione=row['Mod_Versione'],
-                mod_serie=mod_serie, mod_allestimento=row['Mod_Allestimento'], mod_stage=mod_stage,
-                mod_potenza=row['Mod_Potenza'], description=row['Modello_Descriz_Originale'], is_ai="Yes"
-            )
-
-            additional_fields = get_ai_vehicles_organized_fields(
-                brand_record_id=brand_record_id, family_record_id=family_record_id,
-                id_record_model=id_record_model, accuracy=accuracy
-            )
-
-            append_to_vehicles(
-                vehicles_list=agent_ai_vehicles_organized, cod_veicolo=row['CodVeicolo'], year=year, brand=brand,
-                modello_a_serie=modello_a_serie, modello_b_versione=modello_b_versione,
-                mod_verificato=row['Mod_Verificato'], family_name=family_name, mod_versione=row['Mod_Versione'],
-                mod_serie=mod_serie, mod_allestimento=row['Mod_Allestimento'], mod_stage=mod_stage,
-                mod_potenza=row['Mod_Potenza'], description=row['Modello_Descriz_Originale'], is_ai="Yes",
-                additional_fields=additional_fields
-            )
-    except Exception as e:
-        print(f"Exception during zip loop: {str(e)}")
 
 
 def extract_digits_from_modello_b_versione(input_string) -> tuple:
@@ -348,6 +225,46 @@ def extract_digits_from_modello_b_versione(input_string) -> tuple:
             # Check if the word contains only letters (pure string)
             elif word.isalpha():
                 strings_list.append(word)  # Append the word to the strings_list
+    if "AMG" in strings_list and "GT" in strings_list:
+        strings_list.remove("AMG")
+
+    return digits_list, strings_list  # Return both lists as a tuple
+
+
+def extract_strings_and_digits_for_family(input_string):
+    digits_list = []
+    strings_list = []
+
+    if input_string:
+        words = input_string.split()
+
+        for word in words:
+            word_lower = word.lower()
+
+            # Check if the word contains only digits
+            if word.isdigit():
+                digits_list.append(int(word))
+
+            # Check if the word contains only letters (pure string)
+            elif word.isalpha():
+                strings_list.append(word)  # Append the word to the strings_list
+
+            # Check if the word has the pattern: letters followed by numbers (e.g., G550)
+            elif re.match(r'^[a-zA-Z]+\d+$', word_lower):
+                # Extract the numeric part from the word and append it to the list
+                numbers_part = ''.join(re.findall(r'\d+', word))
+                letters_part = ''.join(re.findall(r'[a-zA-Z]+', word))
+                digits_list.append(int(numbers_part))
+                strings_list.append(letters_part)
+
+            # Check if the word has the pattern: letters followed by numbers (e.g., G550)
+            elif re.match(r'^\d+[a-zA-Z]+$', word_lower):
+                # Extract the numeric part from the word and append it to the list
+                numbers_part = ''.join(re.findall(r'\d+', word))
+                letters_part = ''.join(re.findall(r'[a-zA-Z]+', word))
+                digits_list.append(int(numbers_part))
+                strings_list.append(letters_part)
+
     if "AMG" in strings_list and "GT" in strings_list:
         strings_list.remove("AMG")
 
@@ -403,22 +320,32 @@ def process_xlsx_files(df, brand_data, df_family, model_data: pd.DataFrame, poss
         family_name, family_record_id, modified_description, family_word, number_part = extract_family(
             modified_description=modified_description, brand_name=brand, year=year, family_data=family_data
         )
-        # if family_name is not None:
-        #     print(f"Original Description: {description}")
-        #     print(f"Extracted Year: {year}")
-        #     print(f"Extracted Brand: {brand}")
-        #     print(f"Extracted Family: {family_name}")
-        #     print(f"Modified Description: {modified_description}\n")
-
-        # print(f"Modified Description: {modified_description}\n")
 
         # TODO: CHECKPOINT till initial working family extraction
+
+        mod_digits, mod_strings = extract_strings_and_digits_for_family(input_string=modified_description)
+
+        if family_name is None:
+            family_name, family_record_id = extract_family_from_model(
+                mod_digits=mod_digits,
+                mod_strings=mod_strings,
+                model_data=model_data,
+                extracted_year=year,
+                extracted_brand=brand
+            )
+            family_word = family_name
+
+        if family_name is None:
+            print(f"Original description: {description}")
+            print(f"Mod Digits: {mod_digits}")
+            print(f"Mod Strings: {mod_strings}")
 
         model_version_string = extract_model_part_b(
             description=description, subtraction_list=[year, brand, family_name]
         )
 
-        modello_b_versione_digits, modello_b_versione_strings = extract_digits_from_modello_b_versione(input_string=model_version_string)
+        modello_b_versione_digits, modello_b_versione_strings = extract_digits_from_modello_b_versione(
+            input_string=model_version_string)
 
         modello_a_serie, mod_serie, mod_stage, mod_id_record = extract_modello_a_serie_and_stage(
             mod_digits=modello_b_versione_digits, mod_strings=modello_b_versione_strings, model_data=model_data,
@@ -426,12 +353,8 @@ def process_xlsx_files(df, brand_data, df_family, model_data: pd.DataFrame, poss
         )
 
         modello_b_versione = extract_model_part_b(
-            description=description, subtraction_list=[year, brand, family_word, modello_a_serie]
+            description=modified_description, subtraction_list=[modello_a_serie]
         )
-
-        # modello_a_serie, id_record_model, mod_serie, mod_stage = extract_modello_a_serie(
-        #     modified_description=modified_description,
-        #     family_name=family_name, brand_name=brand, year=year, model_data=model_data, number_part=number_part)
 
         accuracy = calculate_accuracy(brand, family_name, modello_a_serie, year, brand_record_id, family_record_id,
                                       mod_id_record, mod_serie, mod_stage)
